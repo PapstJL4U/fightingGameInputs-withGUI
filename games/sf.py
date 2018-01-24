82eaf6b4eae64352256f873fb1428de78c6b3cf6
Jon Farrell <Jon.Farrell731@gmail.com>
2018-01-23 22:25:56 -0500

    Initial Switch to dynamically getting inputs from image maps

---
+++ games/sf.py
@@ -0,0 +1,14 @@
+from os import sep
+
+import standard_directionals
+import complex_directionals
+import extra_notations
+import lmhp_lmhk
+import general_special
+
+_inputs = {}
+_inputs.update(standard_directionals._inputs)
+_inputs.update(complex_directionals._inputs)
+_inputs.update(extra_notations._inputs)
+_inputs.update(lmhp_lmhk._inputs)
+_inputs.update(general_special._inputs)
